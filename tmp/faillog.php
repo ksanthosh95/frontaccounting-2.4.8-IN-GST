2e3dfe2bfa041dc9a8f937e50deeec354ecb9347
ksanthosh95 <ksanthosh95@gmail.com>
2020-09-20 00:50:57 +0530

    Indian GST added

---
+++ tmp/faillog.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Login attempts info.
+*/
+$login_faillog = array (
+  1 => 
+  array (
+    '::1' => 0,
+    'last' => '',
+  ),
+  0 => 
+  array (
+    '::1' => 0,
+    'last' => '',
+  ),
+);
